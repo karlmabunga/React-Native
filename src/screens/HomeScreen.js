5f33b6530f7c8c1afcac3bc2b609eed68417adf8
Karl Mabunga <karlmabunga@yahoo.com>
2021-03-27 21:13:54 -0700

    Added navigations to the homescreen for the components demo

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,14 +1,18 @@
 import React from "react";
-import { Text, StyleSheet, View, Button } from "react-native";
+import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
 
-const HomeScreen = () => {
+const HomeScreen = (props) => {
+
   return (
     <View>
       <Text style={styles.text}>Hello there</Text>
       <Button
-        onPress={() => console.log('Button Pressed')}
+        onPress={() => props.navigation.navigate('Components')}
         title="Go to Components Demo"
       />
+      <TouchableOpacity onPress={() => console.log('List Pressed')}>
+        <Text>Go to List Demo</Text>
+      </TouchableOpacity>
     </View>
   );
 };
