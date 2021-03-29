1aa99b53bb14826d7bdc513f72cec1a60bbbbdeb
Karl Mabunga <karlmabunga@yahoo.com>
2021-03-28 22:17:16 -0700

    Used useState for the counter screen. Able to change state with onPress functions

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text, StyleSheet, Button } from 'react-native';
 
 const CounterScreen = (props) => {
+  const [counter, setCounter] = useState(0);
+
   return (
     <View>
-      <Text>Current Count:</Text>
+      <Text>Current Count: {counter}</Text>
+      <Button title='Increase' onPress={() => {
+        setCounter(counter + 1);
+      }}/>
+      <Button title='Decrease' onPress={() => {
+        setCounter(counter - 1);
+      }}/>
     </View>
   )
 };
