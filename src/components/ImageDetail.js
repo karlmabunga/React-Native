3eaf92165c80691208aa972f64ec4f2b6a7d0733
Karl Mabunga <karlmabunga@yahoo.com>
2021-03-28 22:07:14 -0700

    Added a Counter screen and passed score down into each Image Detail

--- src/components/ImageDetail.js
+++ src/components/ImageDetail.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { View, Text, StyleSheet, Image } from 'react-native';
 
-const ImageDetail = ({ title, imageSource}) => {
+const ImageDetail = ({ title, imageSource, score}) => {
   return (
     <View>
       <Image source={imageSource} />
       <Text>
         {title}
       </Text>
+      <Text>Image Score - {score}</Text>
     </View>
   )
 };
