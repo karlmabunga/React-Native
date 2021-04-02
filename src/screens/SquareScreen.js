6180f1863a50f6ecf42b66742d59cf5086d78fe7
Karl Mabunga <karlmabunga@yahoo.com>
2021-04-02 00:56:48 -0700

    Passed props for which color should be increased or decreased

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import ColorCounter from '../components/ColorCounter';
 
 const SquareScreen = () => {
   return <Text>
-    SquareScreen
+    <ColorCounter color='Red'/>
+    <ColorCounter color='Blue'/>
+    <ColorCounter color='Green'/>
   </Text>
 };
 
