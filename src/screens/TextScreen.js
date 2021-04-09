1b80783733b27200540bb3282119e4e049237b47
Karl Mabunga <karlmabunga@yahoo.com>
2021-04-08 21:11:03 -0700

    Conditionally render a message for text screen

--- src/screens/TextScreen.js
+++ src/screens/TextScreen.js
@@ -2,18 +2,20 @@ import React, { useState } from 'react';
 import { View, Text, StyleSheet, TextInput } from 'react-native';
 
 const TextScreen = () => {
-  const [name, setName] = useState('');
+  const [password, setPassword] = useState('');
 
 
   return (
     <View>
+      <Text>Enter password:</Text>
       <TextInput
         style={styles.input}
         autoCapitalize='none'
         autoCorrect={false}
-        value={name}
-        onChangeText={(newValue) => setName(newValue)}
+        value={password}
+        onChangeText={(newValue) => setPassword(newValue)}
       />
+      { password.length < 4 ? <Text>Password must be 5 characters</Text>: null }
     </View>
   )
 };
