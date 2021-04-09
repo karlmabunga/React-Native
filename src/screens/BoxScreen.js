0efb9fee1918d815439c43495e2104ef5a4ef54c
Karl Mabunga <karlmabunga@yahoo.com>
2021-04-08 21:29:54 -0700

    align items center in box screen

--- src/screens/BoxScreen.js
+++ src/screens/BoxScreen.js
@@ -4,12 +4,24 @@ import { Text, View, StyleSheet } from 'react-native';
 const BoxScreen = () => {
 
   return (
-    <View>
-      <Text>Box Screen</Text>
+    <View style={styles.viewStyle}>
+      <Text style={styles.textStyle}>Child #1</Text>
+      <Text style={styles.textStyle}>Child #2</Text>
+      <Text style={styles.textStyle}>Child #3</Text>
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  viewStyle: {
+    borderWidth: 3,
+    borderColor: 'black',
+    alignItems: 'center'
+  },
+  textStyle: {
+    borderWidth: 3,
+    borderColor: 'red',
+  }
+});
 
 export default BoxScreen;
