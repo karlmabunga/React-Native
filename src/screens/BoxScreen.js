38177705a9da360373560fee62e99b3485850026
Karl Mabunga <karlmabunga@yahoo.com>
2021-04-08 21:17:31 -0700

    Added a box screen for the box object model

---
+++ src/screens/BoxScreen.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+
+const BoxScreen = () => {
+
+  return (
+    <View>
+      <Text>Box Screen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default BoxScreen;
